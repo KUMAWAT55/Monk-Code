94fcb1f3622640ed801ad4c7f28214ef986bfad7
kumawat55 <rhtkumawat55@gmail.com>
2018-01-11 18:09:22 +0530

    Update

---
+++ Hackerrank/Python/Arithmatic_op.py
@@ -0,0 +1,8 @@
+import math
+a=int (raw_input())
+b=int (raw_input())
+if 1<=a<=pow(10,10):
+        if 1<=b<=pow(10,10):
+                    print a+b
+                            print a-b
+                                    print a*b
