dac1e30ca7d76c3aca6aa4686ac2a4d70eed14d4
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-14 22:07:57 +0530

    update

--- Hackerrank/Python/Python-if-else.py
+++ Hackerrank/Python/Python-if-else.py
@@ -1,1 +1,12 @@
+#!/bin/python
+n=input()
+if 1<=n<=100:
+   if n%2==1:
+      print "Weird"
+   elif n%2==0 and 2<n<5:
+      print "Not Weird"
+   elif n%2==0 and 6<n<=20:
+      print "Weird"
+   elif n%2==0 and n>20:
+      print "Not Weird"
 
