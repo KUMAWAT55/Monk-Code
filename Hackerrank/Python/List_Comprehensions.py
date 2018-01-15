7c8c4d6044d53d51e64a3fc03cad39a6d490669a
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-15 16:04:51 +0530

    update

--- Hackerrank/Python/List_Comprehensions.py
+++ Hackerrank/Python/List_Comprehensions.py
@@ -1,1 +1,6 @@
-
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x=input()
+y=input()
+z=input()
+n=input()
+print([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a+b+c!=n])
