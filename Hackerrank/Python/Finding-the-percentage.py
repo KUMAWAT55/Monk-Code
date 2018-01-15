70ecaf5a2367f8e54ac1b8867312db3dd203ecb6
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-15 16:01:40 +0530

    update

--- Hackerrank/Python/Finding-the-percentage.py
+++ Hackerrank/Python/Finding-the-percentage.py
@@ -1,1 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=input()
+dic={}
+lis=[]
+for i in range(n):
+    a=raw_input()
+    lis=a.split()
+    d=lis[0]
+    lis.remove(lis[0])
+    lst1=list(map(float,lis))
+    dic[d]=lst1
+name=raw_input()
+if name in dic:
+    marks=dic[name]
+t=0
+for i in marks:
+    t=t+i
+t=t/len(marks)
+print "%.2f" %t
+
+
 
