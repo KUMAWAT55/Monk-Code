71e5082dabaa1934642ee221d01a586f2332fbea
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-15 16:02:45 +0530

    update

--- Hackerrank/Python/nested_list.py
+++ Hackerrank/Python/nested_list.py
@@ -1,1 +1,7 @@
-
+l=[]
+for _ in range(0,int(input())):
+    l.append([input(), float(input())])
+s_h=sorted(list(set([marks for name,marks in l])))[1]
+for a,b in sorted(l):
+  if b==s_h:
+    print(a)
