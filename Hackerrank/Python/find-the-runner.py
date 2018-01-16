cc373fae175efba08c70b146e84186536422d1d7
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-16 23:02:38 +0530

    update

--- Hackerrank/Python/find-the-runner.py
+++ Hackerrank/Python/find-the-runner.py
@@ -1,1 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#!/usr/bin/python
+x = []
+n=input()
+a=raw_input()
+ll=[]
+ll=map(int,a.split())
+lll=len(ll)
+if lll==n:
+   a=max(ll)
+   ll.remove(max(ll))
+   while a==max(ll):
+      ll.remove(max(ll))
+   print max(ll)
+
+
 
