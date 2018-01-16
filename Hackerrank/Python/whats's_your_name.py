889945aa8f527e0ee720da646005ecc62eb80e41
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-16 23:08:39 +0530

    update

--- Hackerrank/Python/whats's_your_name.py
+++ Hackerrank/Python/whats's_your_name.py
@@ -1,1 +1,7 @@
-
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+f=(raw_input())
+l=(raw_input())
+if len(f)<=10:
+    if len(l)<=10:
+        print "Hello",f,l+"!",
+        print "You just delved into python."
