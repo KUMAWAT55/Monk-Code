971bcce246158dc40ab59fd80a77c2f7b408aff9
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-18 19:08:02 +0530

    update

--- Hackerrank/Python/String-Formatting.py
+++ Hackerrank/Python/String-Formatting.py
@@ -1,1 +1,4 @@
-
+n=int(input())
+w=len(bin(n)[2:])
+for i in range(1,n+1):
+   print (str(i).rjust(w,' '),str(oct(i)[2:]).rjust(w,' '),str(hex(i)[2:].upper()).rjust(w,' '),str(bin(i)[2:]).rjust(w,' '),sep=' ')
