9ca307e84e6d7b7577376ca48876dde9a1c3fec6
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-19 23:15:04 +0530

    update

--- Hackerrank/Python/Introduction-To-sets.py
+++ Hackerrank/Python/Introduction-To-sets.py
@@ -1,1 +1,9 @@
-
+n=int(input())
+arr=set(map(int,input().split()))
+def average(arr):
+    sum=0
+    # your code goes here
+    for i in arr:
+        sum=sum+i
+    return sum/len(arr)
+print(average(arr))
