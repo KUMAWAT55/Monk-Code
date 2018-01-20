dcd4bee25c5960d814d66ac42b23b4d28f4972a3
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-20 11:04:36 +0530

    update

--- Hackerrank/Python/Integer-comes-in-all-sizes.py
+++ Hackerrank/Python/Integer-comes-in-all-sizes.py
@@ -1,1 +1,10 @@
-
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(raw_input())
+b=int(raw_input())
+c=int(raw_input())
+d=int(raw_input())
+if 1<=a<=1000:
+    if 1<=b<=1000:
+        if 1<=c<=1000:
+            if 1<=d<=1000:
+                print pow(a,b)+pow(c,d)
