ed017c1866b7de474346bb36e3b582ad0bfdae09
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-26 23:38:13 +0530

    Basic-Programming

--- HackerEarth/Basic-Programming/Roy_And_Profile.py
+++ HackerEarth/Basic-Programming/Roy_And_Profile.py
@@ -1,1 +1,10 @@
-
+    L=int(input())
+    N=int(input())
+    for i in range(N):
+      W,H=map(int,input().split())
+      if (W<L or H<L):
+        print ("UPLOAD ANOTHER")
+      elif(W==H):
+        print("ACCEPTED")
+      else:
+        print("CROP IT")
