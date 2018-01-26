365093d32396f993f8905e505537daf2908a1bd6
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-26 23:39:24 +0530

    Basic-Programming

--- HackerEarth/Basic-Programming/Prime_Number.py
+++ HackerEarth/Basic-Programming/Prime_Number.py
@@ -1,1 +1,15 @@
-
+    def isPrime(x):
+        if x<2:
+            return 0
+        for i in range(2,x):
+            if not x%i:
+               return 0
+        return 1
+     
+    n=int(input())
+    l=[]
+    for j in range(2,n):
+      
+      if isPrime(j)==1:
+        l.append(j)
+    print (' '.join(repr(e) for e in l))
