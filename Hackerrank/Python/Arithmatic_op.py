a4e668800ef1761e78aebf9a3b1ae8ef3fae5fcb
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-03 22:49:11 +0530

    Introduction Challenges

--- Hackerrank/Python/Arithmatic_op.py
+++ Hackerrank/Python/Arithmatic_op.py
@@ -4,6 +4,6 @@ a=int (raw_input())
 b=int (raw_input())
 if 1<=a<=pow(10,10):
         if 1<=b<=pow(10,10):
-                    print a+b
+                print a+b
                             print a-b
                                     print a*b
