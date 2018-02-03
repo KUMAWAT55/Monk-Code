d19355cd270cfca862dc948231f24352e0d88ae0
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-03 22:58:40 +0530

    Introduction Challenges

--- Hackerrank/Python/Python-if-else.py
+++ Hackerrank/Python/Python-if-else.py
@@ -1,13 +1,11 @@
-#!/bin/python
-##############
-n=input()
+# ---------> PYTHON 3 <---------
+n=int(input())
 if 1<=n<=100:
    if n%2==1:
-      print "Weird"
+      print ("Weird")
    elif n%2==0 and 2<n<5:
-      print "Not Weird"
+      print ("Not Weird")
    elif n%2==0 and 6<n<=20:
-      print "Weird"
+      print ("Weird")
    elif n%2==0 and n>20:
-      print "Not Weird"
-
+      print ("Not Weird")
