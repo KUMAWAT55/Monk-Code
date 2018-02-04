fb9b5b8c72d54d50b12bef22c02c50924d40c21d
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-04 23:23:57 +0530

    Strings Challenges

--- Hackerrank/Python/whats's_your_name.py
+++ Hackerrank/Python/whats's_your_name.py
@@ -1,7 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-f=(raw_input())
-l=(raw_input())
+#----------> PYTHON 3 <----------
+f=(input())
+l=(input())
 if len(f)<=10:
     if len(l)<=10:
-        print "Hello",f,l+"!",
-        print "You just delved into python."
+        print ("Hello",f,l+"!","You just delved into python.")
+        
