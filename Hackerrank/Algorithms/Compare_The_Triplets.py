b92da910bd6623b8825dc8a0b9bc234c82e4c3ce
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-04 23:18:03 +0530

    Warmup Challenges

--- Hackerrank/Algorithms/Compare_The_Triplets.py
+++ Hackerrank/Algorithms/Compare_The_Triplets.py
@@ -1,9 +1,10 @@
-#!/bin/python
+#!/bin/python3
+#----------> PYTHON 3 <----------
 b=0
 a=0
-a0,a1,a2 = raw_input().strip().split(' ')
+a0,a1,a2 = input().strip().split(' ')
 a0,a1,a2 = [int(a0),int(a1),int(a2)]
-b0,b1,b2 = raw_input().strip().split(' ')
+b0,b1,b2 = input().strip().split(' ')
 b0,b1,b2 = [int(b0),int(b1),int(b2)]
 if(a0>b0):
     a=a+1
@@ -18,6 +19,6 @@ if(a2>b2):
 if(b2>a2):
     b=b+1
     
-print a,b
+print (a,b)
             
 
