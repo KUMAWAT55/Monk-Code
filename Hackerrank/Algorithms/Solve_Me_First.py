830d8127e13f83ce3fa3d7c9af96fead146b1b8c
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-04 23:10:05 +0530

    Warmup Challenges

--- Hackerrank/Algorithms/Solve_Me_First.py
+++ Hackerrank/Algorithms/Solve_Me_First.py
@@ -1,7 +1,8 @@
+#----------> PYTHON 3 <----------
 def solveMeFirst(a,b):
     return a+b  
-num1 = input()
-num2 = input()
+num1 = int(input())
+num2 = int(input())
 res = solveMeFirst(num1,num2)
-print res
+print (res)
 
