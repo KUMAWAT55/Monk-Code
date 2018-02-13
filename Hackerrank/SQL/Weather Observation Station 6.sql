427189a27685dc089470e8f6805ed7a53e2ebdc9
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-13 23:10:38 +0530

    Basic Select Challenges

--- Hackerrank/SQL/Weather Observation Station 6.sql
+++ Hackerrank/SQL/Weather Observation Station 6.sql
@@ -1,1 +1,4 @@
-
+/*ORACLE*/
+SELECT DISTINCT city FROM station WHERE city LIKE 'A%' or city LIKE 'E%' or city LIKE 'I%' or city LIKE 'O%' or city LIKE 'U%';
+/*SQL*/
+SELECT DISTINCT city FROM station WHERE city LIKE 'A%' or city LIKE 'E%' or city LIKE 'I%' or city LIKE 'O%' or city LIKE 'U%';
