4e5fff2a147ba5b3692c4490d0ecefbfe5954fdc
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-16 23:05:46 +0530

    update

--- HackerEarth/Basic-Programming/Find Product.py
+++ HackerEarth/Basic-Programming/Find Product.py
@@ -1,1 +1,8 @@
-
+    n=int(raw_input())
+    str=map(int,raw_input().split())
+    ans=1
+    import math
+    if 1<=n<=1000 and 1<=len(str)<=1000:
+        for i in range(len(str)):
+            ans=(ans*str[i])%(10**9+7)
+        print ans
