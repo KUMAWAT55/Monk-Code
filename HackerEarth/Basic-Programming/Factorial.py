b6f30a3d5c3566cef80545b9b1f5a2a803fc860c
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-16 23:18:15 +0530

    Basics of Input/Output

--- HackerEarth/Basic-Programming/Factorial.py
+++ HackerEarth/Basic-Programming/Factorial.py
@@ -1,8 +1,8 @@
 # ----------> PYTHON 3 <----------
-    def fact(x):
-        if x==0:
-            return 1
-        else:
-            return(x*fact(x-1))
-    x=int(input())
-    print(fact(x))
+def fact(x):
+    if x==0:
+        return 1
+    else:
+        return(x*fact(x-1))
+x=int(input())
+print(fact(x))
